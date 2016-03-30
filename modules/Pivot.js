0ef7b5278edce7ec436a1dac24f24ecbb5d3392c
Jorge Alarcon <jorge@zoomdata.com>
2016-03-30 16:03:15 -0400

    Fixed table sorting

--- modules/Pivot.js
+++ modules/Pivot.js
@@ -37,10 +37,10 @@ export default class Pivot extends Component {
 				addr_state: item.group[2],
 				// emp_length: item.group[3],
 				// issue_d: item.group[4],
-				calc_o_s_principal: numeral(item.current.metrics.calc_o_s_principal.calc).format('0,0'),
-				calc_o_s: numeral(item.current.metrics.calc_o_s.calc).format('0,0'),
-				calc_portfolio: numeral(item.current.metrics.calc_portfolio.calc).format('0,0'),
-				calc_avg_size: numeral(item.current.metrics.calc_avg_size.calc).format('0,0')
+				calc_o_s_principal: item.current.metrics.calc_o_s_principal.calc,
+				calc_o_s: item.current.metrics.calc_o_s.calc,
+				calc_portfolio: item.current.metrics.calc_portfolio.calc,
+				calc_avg_size: item.current.metrics.calc_avg_size.calc
 			};
 		});
 
@@ -63,6 +63,7 @@ export default class Pivot extends Component {
 	        rowCount: allOfTheData.length,
 	        pageSize: pageSize,
 	        getRows: function (params) {
+                allOfTheData = this.sortAndFilter(params.sortModel, params.filterModel);
 	            var rowsThisPage = allOfTheData.slice(params.startRow, params.endRow);
                 var lastRow = -1;
                 if (allOfTheData.length <= params.endRow) {
@@ -70,8 +71,40 @@ export default class Pivot extends Component {
                 }
 
                 params.successCallback(rowsThisPage, lastRow);
-	        }
+	        },
+            sortAndFilter: function(sortModel, filterModel) {
+                return this.sortData(sortModel, allOfTheData);
+            },
+            sortData: function(sortModel, data) {
+                var sortPresent = sortModel && sortModel.length > 0;
+                if (!sortPresent) {
+                    return data;
+                }
+                // do an in memory sort of the data, across all the fields
+                var resultOfSort = data.slice();
+                resultOfSort.sort(function(a,b) {
+                    for (var k = 0; k < sortModel.length; k++) {
+                        var sortColModel = sortModel[k];
+                        var valueA = a[sortColModel.colId];
+                        var valueB = b[sortColModel.colId];
+                        // this filter didn't find a difference, move onto the next one
+                        if (valueA==valueB) {
+                            continue;
+                        }
+                        var sortDirection = sortColModel.sort === 'asc' ? 1 : -1;
+                        if (valueA > valueB) {
+                            return sortDirection;
+                        } else {
+                            return sortDirection * -1;
+                        }
+                    }
+                    // no filters found a difference
+                    return 0;
+                });
+                return resultOfSort;
+            }
 	    };
+
 	    this.api.setDatasource(dataSource);
 	}
 
@@ -80,10 +113,10 @@ export default class Pivot extends Component {
 	    createNewDatasource();
 	}
 
-   createColDefs() {
+    createColDefs() {
         const leftAlignStyle = {
             'text-align': 'left'
-        };
+        }
         const centerAlignStyle = {
             'text-align': 'center'
         }
@@ -93,6 +126,9 @@ export default class Pivot extends Component {
         const makeAlignStyle = {
             'border-left': '1px solid #808080',
             'text-align': 'left'
+        }
+        const numberRenderer = function(params) {
+            return numeral(params.value).format('0,0');
         }
         const columnDefs = [
             {
@@ -116,7 +152,7 @@ export default class Pivot extends Component {
             {
                 headerName: 'Lendee State',
                 field: 'addr_state',
-                width: 100,
+                width: 120,
                 suppressSorting: false,
                 suppressSizeToFit: true,
                 suppressMenu: true,
@@ -144,38 +180,42 @@ export default class Pivot extends Component {
                 headerName: 'O/S Principal',
                 field: 'calc_o_s_principal',
                 width: 150,
-                suppressSorting: true,
+                suppressSorting: false,
                 suppressSizeToFit: true,
                 suppressMenu: true,
-                cellStyle: rightAlignStyle
+                cellStyle: rightAlignStyle,
+                cellRenderer: numberRenderer
             }
             ,
             {
                 headerName: '% O/S',
                 field: 'calc_o_s',
                 width: 150,
-                suppressSorting: true,
+                suppressSorting: false,
                 suppressSizeToFit: true,
                 suppressMenu: true,
-                cellStyle: rightAlignStyle
+                cellStyle: rightAlignStyle,
+                cellRenderer: numberRenderer
             },
             {
                 headerName: '% Portfolio',
                 field: 'calc_portfolio',
                 width: 150,
-                suppressSorting: true,
+                suppressSorting: false,
                 suppressSizeToFit: true,
                 suppressMenu: true,
-                cellStyle: rightAlignStyle
+                cellStyle: rightAlignStyle,
+                cellRenderer: numberRenderer
             },
             {
                 headerName: 'Avg Size',
                 field: 'calc_avg_size',
                 width: 150,
-                suppressSorting: true,
+                suppressSorting: false,
                 suppressSizeToFit: true,
                 suppressMenu: true,
-                cellStyle: centerAlignStyle
+                cellStyle: centerAlignStyle,
+                cellRenderer: numberRenderer
             }
         ];
         return columnDefs;
@@ -196,13 +236,13 @@ export default class Pivot extends Component {
 
 	        	<div    
 	                    className='ag-fresh'
-	                    style={{height: 530}}
+	                    style={{height: 570}}
 	            >
 			  		<AgGridReact 
 			  			rowData={allOfTheData}
 			  			onGridReady={this.onGridReady.bind(this)}
 						columnDefs={columnDefs}
-	                    enableSorting='true'
+	                    enableServerSideSorting='true'
 	                    enableFilter='false'
 	                    headerHeight='55'
 	                    rowHeight='28'
